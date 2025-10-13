5c4af50aac0cf6cd992d5501d93816d061407dc6
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-10-13 15:22:17 +0300

    fix(course): Update button href in course page for correct navigation
    - Changed the button href in the course page from a localized path to a direct course ID path, ensuring proper navigation for users.

--- app/[lang]/@student/course/page.tsx
+++ app/[lang]/@student/course/page.tsx
@@ -83,12 +83,7 @@ export default function Page() {
             key={i + ""}
             {...{ ...value, id }}
             btn={
-              <Button
-                color="primary"
-                as={Link}
-                href={`/${lang}/course/${id}`}
-                className=""
-              >
+              <Button color="primary" as={Link} href={`/${id}`} className="">
                 {lang == "en" ? "Enroll" : "ይመዝገቡ"}
               </Button>
             }
